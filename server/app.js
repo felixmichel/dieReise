bed265b7544939f0281e0759a56906c2e6c550d2
Michael Hunziker <michael.hunziker@youengineering.com>
2016-05-02 13:34:39 +0200

    FIXME's fürs Backend. Können wir am Mittwoch anschauen.

--- server/app.js
+++ server/app.js
@@ -2,24 +2,27 @@ var mongoose = require('mongoose');
 
 var express = require('express');
 var cors = require('cors');
+// FIXME: Remove unused packages
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var eventRouter = require('./routes/eventRouter');
 
-//connect to database
+// FIXME: Extract db stuff to module
 var url = 'mongodb://localhost:27017/dieReise';
 mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    // we're connected!
-    console.log("Connected correctly to server");
+  // we're connected!
+  console.log("Connected correctly to server");
 });
 
 var app = express();
 app.use(cors());
+
+
+// FIXME: Uninstall npm modules (npm uninstall). Also remove folder "views", "public" and "bin" (including contents), remove commented code below
 //
 // // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
@@ -44,6 +47,8 @@ app.use(function(req, res, next) {
 
 // error handlers
 
+// FIXME: Extract error handlers to module
+
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -60,6 +65,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
+  // FIXME: Following two lines should not be needed since we use CORS module
   res.header("Access-Control-Allow-Origin", "http://localhost:9000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
